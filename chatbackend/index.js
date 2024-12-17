52f5a7440b10b0ce7aba1c1967d91a82709e5a63
Vũ Phương Thanh <thanh.vp@dsc.com.vn>
2024-12-17 15:31:55 +0700

    initial commit

---
+++ chatbackend/index.js
@@ -0,0 +1,35 @@
+
+    let socket = io('localhost:9000');
+
+    let form = document.getElementById('form');
+    let myname = document.getElementById('myname');
+    let message = document.getElementById('message');
+    let messageArea = document.getElementById("messageArea");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        if (message.value) {
+            socket.emit('send_name', myname.value);
+            socket.emit('send_message', message.value);
+            message.value = "";
+        }
+    });
+
+    socket.on("send_name", (username) => {
+        console.log(username);
+        let name = document.createElement("p");
+        name.style.backgroundColor = "grey";
+        name.style.width = "100%";
+        name.style.textAlign = "center";
+        name.style.color = "white";
+        name.textContent = username + ":";
+        messageArea.appendChild(name);
+    });
+
+    socket.on("send_message", (chat) => {
+        console.log(chat);
+        let chatContent = document.createElement("p");
+        chatContent.textContent = chat;
+        messageArea.appendChild(chatContent);
+    });
---
+++ chatbackend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const port = 9000;
+const socketio = require('socket.io');
+const io = socketio(server, {
+  cors: {
+    origin: "*",  // Cho phép tất cả các nguồn (hoặc thay thế bằng URL frontend cụ thể, ví dụ: "http://localhost:3000")
+    methods: ["GET", "POST"]
+  }
+});
+
+
+// Socket.io connection events
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('send_name', (username) => {
+        console.log(username);
+        io.emit('send name', username);
+    });
+
+    socket.on('send_message', (chat) => {
+      console.log(chat);
+      io.emit('send message', chat);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Start the server
+server.listen(port, () => {
+    console.log(`Server is listening at the port: ${port}`);
+});
